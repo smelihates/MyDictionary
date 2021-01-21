20b7b7c18ef296f68e8a2920be2d0960fa731b87
smelihates <smelihates@gmail.com>
2021-01-22 01:59:01 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<Tkey, TValue>
+    {
+        Tkey[] _keys;
+        TValue[] _values;
+
+        public MyDictionary()
+        {
+            _keys = new Tkey[0];
+            _values = new TValue[0];
+        }
+
+        public void Add(Tkey key,TValue value)
+        {
+            Tkey[] tempKeys = _keys;
+            TValue[] tempValues = _values;
+
+            _keys = new Tkey[_keys.Length + 1];
+            _values = new TValue[_values.Length + 1];
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                _keys[i] = tempKeys[i];
+                _values[i] = tempValues[i];
+            }
+            _keys[_keys.Length - 1] = key;
+            _values[_values.Length - 1] = value;
+            
+        }
+
+        public int Count 
+        {
+            get { return _keys.Length; } 
+        }
+       
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Dictionary<int, string> dictionary = new Dictionary<int, string>();
+
+            //dictionary.Add(1, "Adana");
+            //dictionary.Add(2, "Adıyaman");
+            //dictionary.Add(3, "Afyon");
+            //dictionary.Add(4, "Ağrı");
+            //dictionary.Add(5, "Amasya");
+            //dictionary.Add(6, "Ankara");
+            //Console.WriteLine(dictionary.Count);
+
+
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+
+            myDictionary.Add(1, "Adana");
+            myDictionary.Add(2, "Adıyaman");
+            myDictionary.Add(3, "Afyon");
+            myDictionary.Add(4, "Ağrı");
+            myDictionary.Add(5, "Amasya");
+            myDictionary.Add(6, "Ankara");
+
+            Console.WriteLine(myDictionary.Count);
+
+
+
+
+        }
+    }
+}
